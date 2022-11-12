22db6550d81dfa6dc299e3e7904f5ec350dfcf7e
giansagun <115687490+giansagun@users.noreply.github.com>
2022-11-12 19:17:02 +0400

    Chapter 3

--- Chapter 3- Data Structures/Exercises/Exercise 6.py
+++ Chapter 3- Data Structures/Exercises/Exercise 6.py
@@ -12,8 +12,10 @@ print(f"{name}, please come to dinner.")
 name = guests[1].title()
 print(f"\nSorry, {name} can't make it to dinner.")
 
+
 del(guests[1])
 guests.insert(1, 'Arjay Visperas')
+
 
 name = guests[0].title()
 print(f"\n{name}, please come to dinner.")
@@ -23,3 +25,22 @@ print(f"{name}, please come to dinner.")
 
 name = guests[2].title()
 print(f"{name}, please come to dinner.")
+
+
+print("\nSorry, we can only invite two people to dinner.")
+
+name = guests.pop()
+print(f"Sorry, {name.title()} I can't invite you to the dinner.")
+
+
+name = guests[0].title()
+print(f"{name}, please come to dinner, you are still invited.")
+
+name = guests[1].title()
+print(f"{name}, please come to dinner, you are still invited.")
+
+del(guests[0])
+del(guests[0])
+
+
+print(guests)
